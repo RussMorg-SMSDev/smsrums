0160eb05e71fa3a4b39cb44dde149300eea5c36b
RussMorg <russell.morgan@smsja.net>
2018-08-27 23:22:58 -0500

    updates nav with hyphen for check-ins

--- keystone.js
+++ keystone.js
@@ -62,7 +62,7 @@ keystone.set('nav', {
 	galleries: 'galleries',
 	enquiries: 'enquiries',
 	users: 'users',
-	userAttendance: 'checkins',
+	userAttendance: 'check-ins',
 });
 
 // Start Keystone to connect to your database and initialise the web server
